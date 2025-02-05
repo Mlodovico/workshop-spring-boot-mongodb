2757da96872dd92be03b4612b879af5a72307cc3
Mlodovico <murilo.lodovico@outlook.com>
2025-02-05 01:38:37 -0300

    feature: first commit

---
+++ src/main/java/com/mlodovico/springmongdb/SpringmongdbApplication.java
@@ -0,0 +1,13 @@
+package com.mlodovico.springmongdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmongdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmongdbApplication.class, args);
+	}
+
+}
