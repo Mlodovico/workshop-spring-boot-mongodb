2a2cf92032f95dc4d447b8a72fa6136f0f4f4445
Mlodovico <murilo.lodovico@outlook.com>
2025-03-05 21:29:32 -0300

    fix: resolve endpoint get user post error and new services

--- src/main/java/com/mlodovico/springmongdb/config/Instantiation.java
+++ src/main/java/com/mlodovico/springmongdb/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.mlodovico.springmongdb.config;
 import com.mlodovico.springmongdb.domains.Post;
 import com.mlodovico.springmongdb.domains.User;
 import com.mlodovico.springmongdb.models.dtos.AuthorDTO;
+import com.mlodovico.springmongdb.models.dtos.CommentDTO;
 import com.mlodovico.springmongdb.repositories.PostRepository;
 import com.mlodovico.springmongdb.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.List;
 import java.util.TimeZone;
 
 @Configuration
@@ -41,6 +43,17 @@ public class Instantiation implements CommandLineRunner {
         Post p2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(u1));
         Post p3 = new Post(null, sdf.parse("29/03/2018"), "Praiou", "Muito bom viajar para praia!", new AuthorDTO(u2));
 
+        CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(u3));
+        CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(u2));
+        CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(u3));
+
+        p1.getComments().addAll(Arrays.asList(c1, c2, c3));
+
         postRepository.saveAll(Arrays.asList(p1, p2, p3));
+
+        u1.getPosts().addAll(Arrays.asList(p1, p2));
+        u2.getPosts().add(p3);
+
+        userRepository.saveAll(Arrays.asList(u1, u2));
     }
 }
--- src/main/java/com/mlodovico/springmongdb/models/dtos/AuthorDTO.java
+++ src/main/java/com/mlodovico/springmongdb/models/dtos/AuthorDTO.java
@@ -2,15 +2,33 @@ package com.mlodovico.springmongdb.models.dtos;
 
 import com.mlodovico.springmongdb.domains.User;
 
+import java.io.Serial;
 import java.io.Serializable;
 
 public class AuthorDTO implements Serializable {
+    @Serial
     private static final long serialVersionUID = 1L;
 
     private String Id;
     private String name;
 
     public AuthorDTO(){
+    }
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public AuthorDTO(User userObj){
