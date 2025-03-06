87632089625f5f10e209f52cbf503f249706971e
Mlodovico <murilo.lodovico@outlook.com>
2025-03-05 21:04:58 -0300

    feature: post referenced to user

--- src/main/java/com/mlodovico/springmongdb/domains/User.java
+++ src/main/java/com/mlodovico/springmongdb/domains/User.java
@@ -1,9 +1,12 @@
 package com.mlodovico.springmongdb.domains;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Document
@@ -15,6 +18,9 @@ public class User implements Serializable {
 
     private String name;
     private String email;
+
+    @DBRef(lazy = true)
+    private List<Post> posts = new ArrayList<>();
 
     public User(String id, String name, String email){
         this.id = id;
